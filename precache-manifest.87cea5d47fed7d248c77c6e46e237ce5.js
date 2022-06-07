6a82eaf30dbd781983c9af72e6f304a13b113533
Jose Ramon <jose.117@outlook.com>
2022-06-06 19:37:29 -0500

    Updates

---
+++ precache-manifest.87cea5d47fed7d248c77c6e46e237ce5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56a9936470a8e415b210",
+    "url": "./static/css/main.5da9a853.chunk.css"
+  },
+  {
+    "revision": "56a9936470a8e415b210",
+    "url": "./static/js/main.56a99364.chunk.js"
+  },
+  {
+    "revision": "8682bfb1bfa4b5a898de",
+    "url": "./static/js/1.8682bfb1.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b7f1128ab3101d633a136274b4c8d195",
+    "url": "./index.html"
+  }
+];
