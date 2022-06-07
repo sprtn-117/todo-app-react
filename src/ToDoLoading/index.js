84d9fb6192e913adc594bf0e88b1611836f6d9cc
Jose Ramon <jose.117@outlook.com>
2022-06-06 22:02:24 -0500

    Componente de spinner loader

---
+++ src/ToDoLoading/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./index.css"
+
+
+
+function ToDoLoading() {
+  return (
+      <div className='container'>
+          <div className='loader'></div>
+      </div>
+  )
+}
+
+export {ToDoLoading}
