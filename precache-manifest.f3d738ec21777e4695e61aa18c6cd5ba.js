cb886bd6369eae62e069af5713fa59eb76da987d
Jose Ramon <jose.117@outlook.com>
2022-06-07 17:04:59 -0500

    Updates

---
+++ precache-manifest.f3d738ec21777e4695e61aa18c6cd5ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d52c9013a04c953e94d5",
+    "url": "./static/css/main.fd4da1e3.chunk.css"
+  },
+  {
+    "revision": "d52c9013a04c953e94d5",
+    "url": "./static/js/main.d52c9013.chunk.js"
+  },
+  {
+    "revision": "f8ad2fd52b8722a45d3a",
+    "url": "./static/js/1.f8ad2fd5.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6c61b5a58e979e5b4c97ea89b4109aca",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.87cea5d47fed7d248c77c6e46e237ce5.js"
+  "./precache-manifest.f3d738ec21777e4695e61aa18c6cd5ba.js"
 );
 
 workbox.clientsClaim();
